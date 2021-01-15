72c974ae70a37ccfe3f292ff827d8bf21232556e
Matthew Mannina <mattematics9@gmail.com>
2021-01-14 22:43:38 -0500

    added auth.signout() to loggedInLinks component

--- src/components/LoggedInLinks.js
+++ src/components/LoggedInLinks.js
@@ -1,10 +1,12 @@
 import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../providers/UserProvider'
+import { auth } from '../firebase/config'
 
 const LoggedInLinks = () => {
     const { setCurrentUser } = useContext(UserContext);
-    const handleClick = () => {
+    const handleClick = async () => {
+        await auth.signOut();
         setCurrentUser(null);
     }
 
